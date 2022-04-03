e70ad80386f1102c0a813c1797453c298aa8e5db
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-04-03 14:47:48 -0700

    refactor

--- src/app/__tests__/generator-test.js
+++ src/app/__tests__/generator-test.js
@@ -4,19 +4,12 @@ import helpers from 'yeoman-test';
 const tmpDir = path.resolve('tmp');
 const generator = path.resolve('src', 'app', 'index.js');
 
-describe('Main Generator', () => {
+test(`Main Generator ${(generator)} runs correctly in ${tmpDir}`, async () => {
   const filepath = 'foo/bar.html';
-  let runResult;
-
-  beforeEach(async () => {
-    runResult = await helpers.create(generator)
-      .inDir(tmpDir)
-      .withPrompts({ filepath })
-      .build()
-      .run();
-  });
-
-  it('runs correctly', async () => {
-    runResult.assertFile(filepath);
-  });
+  const runResult = await helpers.create(generator)
+    .inDir(tmpDir)
+    .withPrompts({ filepath })
+    .build()
+    .run();
+  runResult.assertFile(filepath);
 });
