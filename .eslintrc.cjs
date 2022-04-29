963a9957eb48be2e0a666406f6759a430b97bbbb
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-04-28 19:39:38 -0700

    Use js config for eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+  },
+  extends: ['airbnb-base', 'plugin:flowtype/recommended'],
+  plugins: ['flowtype', 'jest'],
+  rules: {
+    'import/no-webpack-loader-syntax': 0,
+    'import/extensions': ['error', 'ignorePackages'],
+    'import/no-duplicates': ['error', { considerQueryString: true }],
+    'import/no-extraneous-dependencies': ['error', { packageDir: '.' }],
+    'no-confusing-arrow': ['error', { allowParens: true }],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-restricted-exports': 0,
+    'import/prefer-default-export': 0,
+    'object-curly-newline': ['error', { consistent: true }],
+    'no-bitwise': ['error', { int32Hint: true }],
+    'no-mixed-operators': 0,
+  },
+  env: {
+    'jest/globals': true,
+  },
+};
